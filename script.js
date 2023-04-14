341ac576203c0e04f3a8e25a87ecb68dbf273df5
Arief Ditia Faltah <AR2841@PID-PRODUCTENGINEER-AR2841.local>
2023-04-14 07:17:03 +0700

    refactor code structure

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-import { promise } from "./modules/promise.js";
-import { prosmiseAll } from "./modules/promiseAll.js";
-import { getExample } from "./modules/request/example1.js";
-import { getProduct } from "./modules/request/product.js";
+import { promise } from "./modules/examples/promise.js";
+import { prosmiseAll } from "./modules/examples/promiseAll.js";
+import { getExample } from "./modules/services/example.js";
+import { getProduct } from "./modules/services/product.js";
 
 // promise()
 // prosmiseAll()
 getExample()
 getProduct()
-localStorage.setItem("data", "A1A2A3")
