eaab61a6ae608a43409f6e7c4dbd76c5bc585a01
Arief Ditia Faltah <arief.dfaltah@gmail.com>
2023-05-02 10:22:25 +0700

    add login logic with mockapi

---
+++ modules/services/login.js
@@ -0,0 +1,54 @@
+export function login() {
+    //Mengambil element dengan id xhr-5-result
+    const result = document.querySelector("#xhr-5-result")
+
+    //Memanggil function onClickGet dengan argumen dari variable result
+    onClickGet(result)
+    
+    //Memanggil function onClickReset dengan argumen dari variable result
+    onClickReset(result)
+}
+
+function onClickGet(result) {
+    //Inisiasi event listener dengan target dari hasil query selector yang mengambil element demgan id xhr-3
+    document.querySelector('#xhr-5').addEventListener('click', async () => {
+        //Inisiasi pada variable result konten yang berupa text akan diberi nilai kosong
+        let email = document.querySelector('#email').value
+        let password = document.querySelector('#password').value
+
+        console.log(email, password)
+
+        result.textContent = '';
+
+        //Inisiasi try catch strategi untuk menghandle error
+        try {
+            //Inisiasi pemanggilan API
+            let response = await fetch('https://645072b0e1f6f1bb2297ddb1.mockapi.io/api/v1/user')
+            
+            //Handle apabila response tidak oke
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+
+        //     //Handle apabila response oke
+            result.innerHTML = parseResponse(await response.json(), email, password)
+        } catch(error) {
+            // Handle response error
+            result.innerHTML = error
+        }
+    })
+}
+
+function onClickReset(result) {
+    document.querySelector('#reset-5').addEventListener('click', () => {
+        result.textContent = '';
+    });
+}
+
+function parseResponse(responseJSON, email, password) {
+    for (let i=0; i<responseJSON.length; i++) {
+        if (responseJSON[i].email == email && responseJSON[i].password == password)
+        return "LOGIN SUCCESS"
+    }
+    return "LOGIN FAILED"
+}
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import { getExample } from "./modules/services/example.js";
 import { getProduct } from "./modules/services/product.js";
 import { getPokemon } from "./modules/services/pokemon.js";
 import { getPokemonAsyncAwait } from "./modules/services/pokemonAsyncAwait.js";
+import { login } from "./modules/services/login.js";
 
 // promise()
 // prosmiseAll()
@@ -11,3 +12,4 @@ getExample()
 getProduct()
 getPokemon()
 getPokemonAsyncAwait()
+login()
